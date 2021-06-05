c76eb7dcf546f0b614d2d23a67ddfd4746c7bb81
roemiliano <82281961+roemiliano@users.noreply.github.com>
2021-06-05 09:10:26 -0500

    Create Ejercicio.js

---
+++ Sesion_2/Ejercicio_3/Ejercicio.js
@@ -0,0 +1,29 @@
+var day = 2;
+var text;
+
+switch (day) {
+  case 0:
+    text = "Sunday";
+    break;
+  case 1:
+    text = "Monday";
+    break;
+  case 2:
+    text = "Tuesday";
+    break;
+  case 3:
+    text = "Wednesday";
+    break;
+  case 4:
+    text = "Thursday";
+    break;
+  case 5:
+    text = "Friday";
+    break;
+  case 6:
+    text = "Saturday";
+    break;
+  default:
+    text = "Error";
+
+}

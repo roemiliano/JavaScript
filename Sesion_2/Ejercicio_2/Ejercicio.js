3f5ab88dd80d1151a311f98f4d6ad178ada52299
roemiliano <82281961+roemiliano@users.noreply.github.com>
2021-06-05 09:08:44 -0500

    Create Ejercicio.js

---
+++ Sesion_2/Ejercicio_2/Ejercicio.js
@@ -0,0 +1,12 @@
+var time = 13;
+var saludo;
+
+if (time < 12) {
+    saludo = "Buenos días"
+}
+if (time < 20) {
+    saludo = "Buenos tardes"
+}
+if (time >= 20) {
+    saludo = "Buenos noches"
+}

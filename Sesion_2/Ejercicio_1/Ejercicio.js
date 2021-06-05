2c3cf031fa2156a46e64de12dd91ae6b07be44c0
roemiliano <82281961+roemiliano@users.noreply.github.com>
2021-06-05 09:07:28 -0500

    Create Ejercicio.js

---
+++ Sesion_2/Ejercicio_1/Ejercicio.js
@@ -0,0 +1,9 @@
+if (time >= 0 && time < 12 ) {
+    saludo = "Buenos días"
+ } else if (time >= 12 && time < 20) {
+     saludo = "Buenos tardes"
+ } else if (time >= 20 && time < 24) {
+    saludo = "Buenos noches"
+ } else {
+     saludo = "Hora desconocida"
+ }
